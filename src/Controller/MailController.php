eca8afb6028b9f834700358df6d8f4ed84c9925a
jeremy <jeremymartinsio1@gmail.com>
2018-01-25 13:31:51 +0100

    first push commit

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -1,12 +1,34 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: jeremy martin
- * Date: 25/01/2018
- * Time: 12:03
- */
-class MailController
+namespace  App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class MailController extends Controller
 {
 
+
+    public function indexAction(){
+        return new Response("Bienvenu sur l'api mailing traveled ! ");
+    }
+
+    public function sendWelcomeMail(Request $request){
+
+        $message = (new \Swift_Message("Bienvenu sur traveked"))
+                ->setFrom(array("Traveled"=>"travelednoreply@gmail.com"))
+            ->setTo($request->get('to'))
+            ->setBody(
+                $this->renderView(
+                // templates/emails/registration.html.twig
+                    'emails/registration.html.twig',
+                    array('link' => $request->get('link'))
+                ),
+                'text/html'
+            );
+
+        $this->get('mailer')->send($message);
+    }
 }
